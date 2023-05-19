1e57d9ace9e69aa1284008c5282f1fc490811c62
BaselAshraf81 <129132034+BaselAshraf81@users.noreply.github.com>
2023-05-19 20:36:29 +0300

    Update urls.py

--- AsnanDental/urls.py
+++ AsnanDental/urls.py
@@ -20,7 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('website.urls')),
-    static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT),
-static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 ]
-urlpatterns += 
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
